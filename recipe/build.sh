25a15acfb6a6e6e8b6e0fade7e9f94bf0287f64d
Kyle Leaders <remkade@gmail.com>
2022-01-13 10:02:57 -0800

    Using glob to exec installer

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,22 +4,20 @@ set -o pipefail
 set -o xtrace
 set -o nounset
 
-# Untar and delete the version number found between _ and _
-# This is intened to make it simpler to maintain between versions
-# Requires gnutar, so will not work on BSD or mac versions of tar
-tar -xv --transform 's,_[^/]*_manylinux,,g' --show-transformed-names -f arm-performance-libraries.tar
-cd arm-performance-libraries || exit 1
-
 # Run installer script
-./arm-performance-libraries.sh --accept --install-to "${PREFIX}" --force
+# This avoids ShellCheck SC2211: https://github.com/koalaman/shellcheck/wiki/SC2211
+# First expand the list list of possible matches into an array
+INSTALLER=(arm-performance-libraries*.sh)
+# Execute the first script found
+exec "./${INSTALLER[0]}" --accept --install-to "${PREFIX}" --force || exit 1
 
 # Create the standard installation locations
 mkdir "${PREFIX}/include"
 mkdir "${PREFIX}/lib"
 
 # Move everything into the standard locations
-cp -rn "${PREFIX}"/armpl_*_gcc-*/include*/* "${PREFIX}"/include
-cp -rn "${PREFIX}"/armpl_*_gcc-*/lib*/* "${PREFIX}"/lib
+cp -rvn "${PREFIX}"/armpl_*_gcc-*/include*/* "${PREFIX}"/include
+cp -rvn "${PREFIX}"/armpl_*_gcc-*/lib*/* "${PREFIX}"/lib
 
 # Remove arm specific directories
 rm -rf "${PREFIX}"/armpl_*
